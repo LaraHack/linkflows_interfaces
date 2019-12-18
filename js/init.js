738373aad56780b4068b7659bbe1209ee17df397
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2019-12-18 15:32:04 +0100

    fixed color bug when certain dimensions are selected

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
-
+// palette of colors used for all dimensions
 var colors = {article: "#cd853f", section: "#deb887", paragraph: "#ffe4c4",
   syntax: "#c6deff", style: "#82cafa", content:"#9e7bff",
   negative: "#ff0000", neutral: "#ffff00", positive: "#008000",
   I1: "rgba(0, 0, 0, 0)", I2: "rgba(0, 0, 0, 0.25)", I3: "rgba(0, 0, 0, 0.5)", I4: "rgba(0, 0, 0, 0.75)", I5: "rgba(0, 0, 0, 1)",
   compulsory: "#ff6500", suggestion: "#ffa500", no_action: "#ffd700"};
 
+// coloring a span element that acts as part of the legend for the graph
+// from the color palette above, for one part of one dimension
 function setSpanColor(dimName) {
   var spanId = "span" + dimName.charAt(0).toUpperCase() + dimName.slice(1);
   var spanFound = document.getElementById(spanId);
@@ -13,10 +15,13 @@ function setSpanColor(dimName) {
   }
 }
 
+// coloring the separate span elements that act as legend for the graph
+// from the color palette above, for all dimensions
 for (const [key, value] of Object.entries(colors)) {
   setSpanColor(key);
 }
 
+// graph size
 var margin = {top: 10, right: 100, bottom: 30, left: 100},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -72,6 +77,7 @@ d3.csv("Q2.csv", function(error, data) {
   // get the reviewers
   var rowHeaders = d3.keys(data[0]).filter(function(key) { return key !== "Reviewer"; });
 
+  // set color range for the dimensions present in the data file
   var colorRange = [];
 
   for (var i = 0; i < rowHeaders.length; i++) {
