4100bca20a877a898c1974af8c1fc1dbffd870dd
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-22 15:00:19 +0100

    fixing CORS issues

--- test_client.js
+++ test_client.js
@@ -1,13 +1,13 @@
 
-$("#getPicFromServer").on("click", getImageFromServer);
+$("#getMessage").on("click", getMessageFromServer);
 
-function getImageFromServer () {
+function getMessageFromServer () {
 	// var serverConnection = "http://127.0.0.1:8081/images/test.jpeg";
 		var serverConnection = "http://localhost:8081/";
 
-	$.get(serverConnection, function (data) {
-		//$("#testMessage").text(data);
-		alert("Communication with server done: " + data);
+	$.get(serverConnection, function (data, status) {
+		$("#testMessage").text(data);
+		alert("Communication with server done: " + status);
 	});
 	// var net = require('net');
 	//
