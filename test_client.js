bd717bb21586bb6c785b66d161ab939aba99f781
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-22 13:52:27 +0100

    test client to connect to server

--- test_client.js
+++ test_client.js
@@ -2,21 +2,27 @@
 $("#getPicFromServer").on("click", getImageFromServer);
 
 function getImageFromServer () {
+	// var serverConnection = "http://127.0.0.1:8081/images/test.jpeg";
+		var serverConnection = "http://localhost:8081/hello";
 
-	var net = require('net');
-
-	var client = new net.Socket();
-	client.connect(8081, '127.0.0.1', function() {
-		console.log('Connected');
-		client.write('Hello, server! Love, Client.');
+	$.get(serverConnection, function (data) {
+		$("#testMessage").html(data);
+		alert("Communication with server done!");
 	});
-
-	client.on('data', function(data) {
-		console.log('Received: ' + data);
-		client.destroy(); // kill client after server's response
-	});
-
-	client.on('close', function() {
-		console.log('Connection closed');
-	});
+	// var net = require('net');
+	//
+	// var client = new net.Socket();
+	// client.connect(8081, '127.0.0.1', function() {
+	// 	console.log('Connected');
+	// 	client.write('Hello, server! Love, Client.');
+	// });
+	//
+	// client.on('data', function(data) {
+	// 	console.log('Received: ' + data);
+	// 	client.destroy(); // kill client after server's response
+	// });
+	//
+	// client.on('close', function() {
+	// 	console.log('Connection closed');
+	// });
 }
