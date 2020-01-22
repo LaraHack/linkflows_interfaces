3d3443c6b2132b973b31ea6a84a84ec72c340dc6
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-22 20:17:22 +0100

    cleaned a bit the code

--- test_client.js
+++ test_client.js
@@ -2,27 +2,11 @@
 $("#getMessage").on("click", getMessageFromServer);
 
 function getMessageFromServer () {
-	// var serverConnection = "http://127.0.0.1:8081/images/test.jpeg";
-		var serverConnection = "http://localhost:8081/";
+	var serverConnection = "http://localhost:8081/";
 
 	$.get(serverConnection, function (data, status) {
 		$("#testMessage").text(data);
 		$("#testMessage").show();
 	});
-	// var net = require('net');
-	//
-	// var client = new net.Socket();
-	// client.connect(8081, '127.0.0.1', function() {
-	// 	console.log('Connected');
-	// 	client.write('Hello, server! Love, Client.');
-	// });
-	//
-	// client.on('data', function(data) {
-	// 	console.log('Received: ' + data);
-	// 	client.destroy(); // kill client after server's response
-	// });
-	//
-	// client.on('close', function() {
-	// 	console.log('Connection closed');
-	// });
+
 }
