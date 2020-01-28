4ce0abb1c8361a90d3fc4b1c79b504773ea1540c
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-28 14:50:44 +0100

    added test code to connect to Virtuoso instance, not yet working

--- test_client.js
+++ test_client.js
@@ -8,5 +8,23 @@ function getMessageFromServer () {
 		$("#testMessage").text(data);
 		$("#testMessage").show();
 	});
+}
 
+
+function getDataFromVirtuoso () {
+	var serverConnection = "http://localhost:8081/sparql";
+
+	$.get(serverConnection, function (data, status) {
+	    console.log("data:" + dataVirtuoso);
+	    console.log("status:" + status);
+
+	    var data = JSON.parse(dataVirtuoso);
+	    console.log("_____________________");
+	    console.log("json data:");
+	    console.log(data);
+	    console.log("_____________________");
+
+			$("#testData").text(data);
+			$("#testData").show();
+		}
 }
